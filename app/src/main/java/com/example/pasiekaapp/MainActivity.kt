5ae2ba8577ad9b51824d0b78b69fa6b39465d279
jordanrafalm <jordanrafalm@gmail.com>
2024-07-02 13:07:48 +0200

    LoginFragment, RegisterFragment, SplashScreenFragment - layout created

--- app/src/main/java/com/example/pasiekaapp/MainActivity.kt
+++ app/src/main/java/com/example/pasiekaapp/MainActivity.kt
@@ -1,24 +1,40 @@
 package com.example.pasiekaapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pasiekaapp.ui.theme.PasiekaAppTheme
+import androidx.fragment.app.Fragment
+import com.example.pasiekaapp.ui.LoginFragment
+import com.example.pasiekaapp.ui.RegisterFragment
+import com.example.pasiekaapp.ui.SplashScreenFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
 
+        val loginButton: Button = findViewById(R.id.loginButton)
+        val registerButton: Button = findViewById(R.id.registerButton)
+        val splashButton: Button = findViewById(R.id.splashButton)
+
+        loginButton.setOnClickListener {
+            openFragment(LoginFragment())
+        }
+
+        registerButton.setOnClickListener {
+            openFragment(RegisterFragment())
+        }
+
+        splashButton.setOnClickListener {
+            openFragment(SplashScreenFragment())
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
-
